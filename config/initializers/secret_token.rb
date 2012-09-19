cef55d5e823bffd31a5ba68504f34bdbce08bafd
Harshad-Mande <Harshad.Mande@cmu.edu>
2012-09-18 22:24:07 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CliqueSandbox::Application.config.secret_token = '21ac84e964b96819994e64a74843dde3ce8260123c04ff60f558f0b945133a26045cd141490261ea5b610355ac4d61d90781f1efb49e265e8d28e3c1fbf5e3ff'
